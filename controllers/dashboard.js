18f53d56f387096336a194d844ddf9cc743f580a
Michael Louis Langan <20106749@mail.wit.ie>
2026-08-31 23:23:09 +0100

    handlebars

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,29 +2,42 @@
 
 import logger from "../utils/logger.js";
 
-const modlist = [
-  {
-    id: 1,
-    title: "appleskin",
-    author: "dz40"
-  },
-  {
-    id: 2,
-    title: "applecore",
-    author: "dz40"
-  },
-  {
-    id: 3,
-    title: "smoothfontslib",
-    author: "caligrizombie"
-  }
-];
+// const modlist = [
+//   {
+//     id: 1,
+//     title: "appleskin",
+//     author: "dz40"
+//   },
+//   {
+//     id: 2,
+//     title: "applecore",
+//     author: "dz40"
+//   },
+//   {
+//     id: 3,
+//     title: "smoothfontslib",
+//     author: "caligrizombie"
+//   }
+// ];
+
+// const dashboard = {
+//   createView(request, response) {
+//     logger.info("Dashboard page loading!")
+//     logger.debug("Loading the modlist", modlist);
+//     response.json(modlist);   
+//   },
+// };
+
 
 const dashboard = {
   createView(request, response) {
-    logger.info("Dashboard page loading!")
-    logger.debug("Loading the modlist", modlist);
-    response.json(modlist);   
+    logger.info("Dashboard page loading!");
+    
+    const viewData = {
+      title: "Modlist App Dashboard"
+    };
+    
+    response.render('dashboard', viewData);
   },
 };
 
--- controllers/start.js
+++ controllers/start.js
@@ -5,11 +5,21 @@ import appStore from "../models/app-store.js";
 
 const start = {
   createView(request, response) {
-    const info = appStore.getAppInfo();
-    logger.debug(info);
+    // const info = appStore.getAppInfo();
+    // logger.debug(info);
 
     logger.info("Start page loading!");
-    response.send('Welcome to my app!');   
+    // response.send('Welcome to my modlist app!');   
+
+
+    const viewData = {
+      title: "Welcome to my modlist app!",
+      info: appStore.getAppInfo()
+    };
+    
+    //logger.debug(viewData);
+    response.render('start', viewData); 
+
   },
 };
 
